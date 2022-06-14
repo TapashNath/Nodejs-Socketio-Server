c74203fb903645e2ec858151ea129fc1033c2f7d
TapashNath <debnathtapash56@gmail.com>
2022-06-15 02:28:25 +0530

    make it better

--- app.js
+++ app.js
@@ -116,11 +116,11 @@ basketball.on("connection", function (socket) {
   });
 });
 
-http.listen(3000, function () {
-  console.log("listening on *:3000");
-});
+// http.listen(3000, function () {
+//   console.log("listening on *:3000");
+// });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
